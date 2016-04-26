f2517e7506d6f380945d64e98c704d5eb7083dde
Shrujal Shah <shrujalshah@hotmail.com>
2016-04-26 12:25:20 +0530

    Adding new area

---
+++ ErtoOfGujarat/ErtoOfGujarat/Areas/License/Controllers/LearningLicenseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErtoOfGujarat.Areas.License.Controllers
+{
+    public class LearningLicenseController : Controller
+    {
+        // GET: License/LearningLicense
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErtoOfGujarat/ErtoOfGujarat/Areas/License/LicenseAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ErtoOfGujarat.Areas.License
+{
+    public class LicenseAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "License";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "License_default",
+                "License/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
